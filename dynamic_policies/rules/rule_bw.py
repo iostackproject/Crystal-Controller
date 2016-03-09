944c5f8b976a7424d547beb2acb6d059c683ebe3
marsqui <marc.siquier@bsc.es>
2016-03-09 13:26:43 +0100

    BW changes saved to redis, problems with connection to RMQ solved

--- dynamic_policies/rules/rule_bw.py
+++ dynamic_policies/rules/rule_bw.py
@@ -57,14 +57,13 @@ class Rule_Bw(object):
         self.rmq_host = '84.88.51.222'
         self.rmq_port = 5672
         self.rmq_exchange = 'bw_assignations'
-        credentials = pika.PlainCredentials(self.rmq_user, self.rmq_pass)
-        # WHY BLOCKING & ALWAYS OPEN?
-        self._channel = pika.BlockingConnection(pika.ConnectionParameters(
-            host=self.redis_host, credentials=credentials)).channel()
-        self._channel.exchange_declare(exchange=self.rmq_exchange,
-                         exchange_type='topic')
-
-        self.last_bw={}
+        self.credentials = pika.PlainCredentials(self.rmq_user, self.rmq_pass)
+        try:
+            self.r = redis.Redis(connection_pool=redis.ConnectionPool(host=self.redis_host, port=self.redis_port, db=0))
+        except:
+            return Response('Error connecting with DB', status=500)
+        
+        self.last_bw = self.load_last_bw()
 
     def add_metric(self):
         """
@@ -81,43 +80,65 @@ class Rule_Bw(object):
         except:
             raise Exception('Error attaching to metric get_bw_info')
 
+    def load_last_bw(self):
+        last = dict()
+        keys = self.r.keys("last_bw:*")
+        for key in keys:
+            last[key[8:]] = self.r.hgetall(key)
+        return last
+
+    def connect_rmq(self):
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host=self.redis_host, credentials=self.credentials))    
+        self._channel = self._connection.channel()
+        self._channel.exchange_declare(exchange=self.rmq_exchange,
+                             exchange_type='topic')
+
+    def disconnect_rmq(self):
+        self._channel.close()
+        self._connection.close()
+
+    def send_message_rmq(self, message, routing_key):
+        self.connect_rmq()
+        self._channel.basic_publish(exchange=self.rmq_exchange,routing_key=routing_key, body=str(message))
+        self.disconnect_rmq()
 
     def update(self, metric, info):
+        self.info = info
         self.assignations = self.compute_assignations(info)
-        self.send_bw()
+        self.send_bw(self.assignations)
 
     def compute_assignations(self, info):
         """
-        Algoritme d'assignacio de BW
+        BW assignation algorithm
         :return: dict[account][policy]['ips'][ip]:bw
         """
         assign = dict()
+        bw_a = dict()
         bw = self.get_redis_bw()
         for account in info:
             assign[account] = dict()
+            bw_a[account] = dict()
             for ip in info[account]:
                 for policy in info[account][ip]:
                     if not policy in assign[account]:
                         assign[account][policy] = dict()
                     if not 'num' in assign[account][policy]:
                         assign[account][policy]['num'] = 1
-                        assign[account][policy]['real_bw'] = info[account][ip][policy]
                     else:
                         assign[account][policy]['num'] += 1
-                        assign[account][policy]['real_bw'] += info[account][ip][policy]
                     if not 'ips' in assign[account][policy]:
-                        assign[account][policy]['ips'] = dict()
-                    try:
-                        assign[account][policy]['bw'] = int(bw[account][policy])
-                        assign[account][policy]['ips'][ip] = int(bw[account][policy])/assign[account][policy]['num']
-                    except:
-                        assign[account][policy]['bw'] = -1
-                        assign[account][policy]['ips'][ip] = -1
+                        assign[account][policy]['ips'] = set()
+                    assign[account][policy]['ips'].add(ip)
+                    
             for policy in assign[account]:
                 for ip in assign[account][policy]['ips']:
-                    assign[account][policy]['ips'][ip] = int(bw[account][policy])/assign[account][policy]['num']
+                    try:
+                        bw_a[account][ip+"-"+policy] = int(bw[account][policy])/assign[account][policy]['num']
+                    except:
+                        bw_a[account][ip+"-"+policy] = -1
 
-        return assign
+        return bw_a
 
 
     def get_redis_bw(self):
@@ -125,42 +146,43 @@ class Rule_Bw(object):
         Gets the bw assignation from the redis database
         """
         bw = dict()
-        try:
-            r = redis.Redis(connection_pool=redis.ConnectionPool(host=self.redis_host, port=self.redis_port, db=0))
-        except:
-            return Response('Error connecting with DB', status=500)
-        keys = r.keys("bw:*")
+        keys = self.r.keys("bw:*")
         for key in keys:
-            bw[key[3:]] = r.hgetall(key)
+            bw[key[3:]] = self.r.hgetall(key)
         return bw
 
-    def send_bw(self):
+    def send_bw(self, assign):
         routing_key = "."
         address = " "
         send = False
-        for account in self.assignations:
-            for policy in self.assignations[account]:
-                for ip in self.assignations[account][policy]['ips']:
-                    if account in self.last_bw and policy in self.last_bw[account] and ip in self.last_bw[account][policy]['ips']:
-                        if self.assignations[account][policy]['ips'][ip] != self.last_bw[account][policy]['ips'][ip]:
-                            send = True
-                            address = address + ip+'/'+account+'/'+policy+'/'+str(self.assignations[account][policy]['ips'][ip])+'/ '
-                            routing_key = routing_key + ip.replace('.','-').replace(':','-') + "."
-                    else:
+        for account in assign:
+            for ip in assign[account]:
+
+                if account in self.last_bw and ip in self.last_bw[account]:
+                    if int(assign[account][ip]) != int(self.last_bw[account][ip]):
                         send = True
-                        address = address +ip+ "/" + account + "/" + policy + "/" + str(self.assignations[account][policy]['ips'][ip]) + "/ "
-                        routing_key = routing_key + ip.replace('.','-').replace(':','-') + "."
+                        ip_c = ip.split('-')
+                        address = address + ip_c[0]+'/'+account+'/'+ ip_c[1]+'/'+str(assign[account][ip])+'/ '
+                        routing_key = routing_key + ip_c[0].replace('.','-').replace(':','-') + "."
+                        self.r.hset("last_bw:"+account, ip, assign[account][ip])
+                        self.last_bw[account][ip] = assign[account][ip]
+                else:
+                    send = True
+                    ip_c = ip.split('-')
+                    address = address + ip_c[0]+'/'+account+'/'+ ip_c[1]+'/'+str(assign[account][ip])+'/ '
+                    routing_key = routing_key + ip_c[0].replace('.','-').replace(':','-') + "."
+                    self.r.hset("last_bw:"+account, ip, assign[account][ip])
+                    if not account in self.last_bw:
+                        self.last_bw[account] = dict()
+                    self.last_bw[account][ip] = assign[account][ip]
         if send:
             print "BW CHANGED"
-            #Save changes to redis and check when init and when new requests in OSs
-            self._channel.basic_publish(exchange='bw_assignations',routing_key=routing_key, body=str(address))
-            print routing_key
+            self.send_message_rmq(address, routing_key)
             print address
-            self.last_bw = self.assignations
 
     def get_tenant(self):
         """
-        Retrun the tenant assigned to this rule.
+        Return the tenant assigned to this rule.
 
         :return: Return the tenant id assigned to this rule
         :rtype: String type.
