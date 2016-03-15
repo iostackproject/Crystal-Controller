cf95f88b17b978f3c6933958d1e7441f98c93456
marsqui <marc.siquier@bsc.es>
2016-03-15 13:15:17 +0100

    Rule disk stats for SortNodes

--- dynamic_policies/metrics/get_disk_stats.py
+++ dynamic_policies/metrics/get_disk_stats.py
@@ -8,7 +8,7 @@ import requests
 class Get_Disk_Stats(Metric):
     _sync = {}
     _async = ['get_value', 'attach', 'detach', 'notify', 'start_consuming','stop_consuming', 'init_consum', \
-            'stop_actor', 'get_redis_bw', 'compute_assignations', 'parse_osinfo', 'send_bw']
+            'stop_actor', 'join_disk_stats']
     _ref = ['attach', 'detach']
     _parallel = []
 
@@ -24,14 +24,27 @@ class Get_Disk_Stats(Metric):
         print 'Get_Disk_stats initialized'
         self.node_stats = {}
 
+    def attach(self, observer, stats_obs):
+        """
+        Asyncronous method. This method allows to be called remotelly. It is called from
+        observers in order to subscribe in this workload metric. This observer will be
+        saved in a dictionary type structure where the key will be the tenant assigned in the observer,
+        and the value will be the PyActive proxy to connect to the observer.
+
+        :param observer: The PyActive proxy of the oberver rule that calls this method.
+        :type observer: **any** PyActive Proxy type
+        """
+        if stats_obs:
+            self.stats_observer = observer
+
     def notify(self, body):
         try:
-            self.parse_disk_stats(json.loads(body))
-            print self.node_stats
+            self.join_disk_stats(json.loads(body))
+            self.stats_observer.update(self.name, self.node_stats)
         except:
-            pass
+            print "Not stats_observer"
 
-    def parse_disk_stats(self, disk_stats):
+    def join_disk_stats(self, disk_stats):
         for dev in disk_stats:
             self.node_stats[dev] = disk_stats[dev]
 
---
+++ dynamic_policies/rules/rule_stats.py
@@ -0,0 +1,85 @@
+
+from pyactive.controller import init_host, serve_forever, start_controller, interval, sleep
+from pyactive.exception import TimeoutError, PyactiveError
+from collections import OrderedDict
+import requests
+import operator
+import json
+import redis
+import pika
+import logging
+
+mappings = {'>': operator.gt, '>=': operator.ge,
+        '==': operator.eq, '<=': operator.le, '<': operator.lt,
+        '!=':operator.ne, "OR":operator.or_, "AND":operator.and_}
+
+#TODO: Add the redis connection into rule object
+logging.basicConfig(filename='./rule.log', format='%(asctime)s %(message)s', level=logging.INFO)
+
+
+class Rule_Stats(object):
+    """
+    Rule: Each policy of each tenant is compiled as Rule. Rule is an Actor and it will be subscribed
+    in the workloads metrics. When the data received from the workloads metrics satisfies
+    the conditions defined in the policy,the Rule actor executes an Action that it is
+    also defined in the policy.
+    """
+    _sync = {'get_tenant':'2'}
+    _async = ['update', 'start_rule', 'stop_actor', 'add_metric']
+    _ref = []
+    _parallel = []
+
+    def __init__(self, host, host_ip, host_port, host_transport):
+        """
+        Inicialize all the variables needed for the rule.
+
+        :param host: The proxy host provided by the PyActive Middleware.
+        :type host: **any** PyActive Proxy type
+        :param host_ip: The host ip adress.
+        :type host_ip: **any** String type
+        :param host_port: The host port address.
+        :type host_port: **any** Numeric type.
+        :param host_transport: The host transport used for the comunication.
+        :type host_transport: **any** String type.
+
+        """
+        logging.info('Rule init: OK')
+        self.base_uri = host_transport+'://'+host_ip+':'+str(host_port)+'/'
+        self.host = host
+        self.redis_host='84.88.51.222'
+        self.redis_port=6379
+        try:
+            self.r = redis.Redis(connection_pool=redis.ConnectionPool(host=self.redis_host, port=self.redis_port, db=0))
+        except:
+            return Response('Error connecting with DB', status=500)
+
+    def add_metric(self):
+        """
+        The `add_metric()` method subscribes the rule to all workload metrics that it
+        needs to check the conditions defined in the policy
+
+        :param workload_name: The name that identifies the workload metric.
+        :type workload_name: **any** String type
+
+        """
+        try:
+            observer = self.host.lookup(self.base_uri+'metrics.get_disk_stats/Get_Disk_Stats/get_disk_stats')
+            observer.attach(self.proxy, True)
+        except:
+            raise Exception('Error attaching to metric get_disk_stats')
+
+    def update(self, metric, info):
+        self.sorted_devices = self.sort(info)
+
+        
+    def sort(self, devices):
+        return OrderedDict(sorted(devices.items(), key=lambda x: x[1]['mean-1min']))
+
+    def get_tenant(self):
+        """
+        Return the tenant assigned to this rule.
+
+        :return: Return the tenant id assigned to this rule
+        :rtype: String type.
+        """
+        return self.tenant
